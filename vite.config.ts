46384b4139fac1387d5fcdbbb5f29714e8105e05
victor <vicdotexe@gmail.com>
2023-12-22 13:43:42 -0800

    types fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
 import Components from "unplugin-vue-components/vite";
 import path from "path";
-import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    dts({
-      entryRoot: "src",
-      staticImport: true,
-      compilerOptions: {
-        skipLibCheck: true,
-      },
-    }),
     Components({
       dts: false,
       extensions: ["vue", "ts"],
@@ -29,8 +20,10 @@ export default defineConfig({
       name: "TimeBlocksVue",
       // the name of the output files when the build is run
       fileName: "time-blocks-vue",
+      formats: ["es"],
     },
     outDir: "dist",
+    emptyOutDir: false,
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
